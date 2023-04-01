60e0c8acad5c09a13f7e5623d99a1aac6e83a068
Normachoyos <126018449+Normachoyos@users.noreply.github.com>
2023-03-31 21:05:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function info(){alert('Año de egreso 1999\n promedio:10.');}
+              
+function info2(){alert('Año de egreso 2000\n promedio:8.');}
+
+
+function info3(){alert('Año de egreso 2001\n promedio:9.');}
+  
+  function saludar(){alert('hola bienvenido');}
